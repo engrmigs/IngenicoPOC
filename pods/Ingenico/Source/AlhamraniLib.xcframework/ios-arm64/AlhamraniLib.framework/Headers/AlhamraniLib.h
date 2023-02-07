6f747b30a183f53726983e52f5381019775cfd73
engrmigs <72691746+engrmigs@users.noreply.github.com>
2023-02-07 09:58:55 +0300

    Add files via upload

---
+++ pods/Ingenico/Source/AlhamraniLib.xcframework/ios-arm64/AlhamraniLib.framework/Headers/AlhamraniLib.h
@@ -0,0 +1,18 @@
+//
+//  AlhamraniLib.h
+//  AlhamraniLib
+//
+//  Created by c7121 on 06/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlhamraniLib.
+FOUNDATION_EXPORT double AlhamraniLibVersionNumber;
+
+//! Project version string for AlhamraniLib.
+FOUNDATION_EXPORT const unsigned char AlhamraniLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlhamraniLib/PublicHeader.h>
+
+
